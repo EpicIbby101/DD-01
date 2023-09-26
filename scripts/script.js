686a15d70db2d91f93e8b04119876434d2e350d3
EpicIbby101 <kevin_grittner@outlook.com>
2023-09-27 00:08:31 +0100

    Refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,207 +1,146 @@
 function initMap() {
-  $(document).ready(function () {
-    // Initialize the Google Maps Places service
-    // Declare default latitude, longitude, and zoom values
-    const map = new google.maps.Map($("#map")[0], {
-      center: { lat: 40.7128, lng: -74.006 },
-      zoom: 15,
+$(document).ready(function () {
+  const mapOptions = {
+    center: { lat: 40.7128, lng: -74.006 },
+    zoom: 15,
+  };
+  
+
+  const map = new google.maps.Map($("#map")[0], mapOptions);
+  const placesService = new google.maps.places.PlacesService(map);
+  
+
+  // Function to create and append a list of places
+  function createPlaceList(container, results, itemClass, itemHeight, title) {
+    container.empty();
+    const row = $("<div class='row'></div>");
+
+    $.each(results, function (index, place) {
+      const col = $(`<div class='col-md-4'></div>`);
+      const item = $(`<div class='${itemClass}'></div>`);
+      item.css("height", itemHeight);
+
+      const name = place.name;
+      const address = place.vicinity;
+      const rating = place.rating;
+      const photos = place.photos;
+
+      // Create and append place list items
+      const listItem = $("<div class='restaurant-item'></div>");
+
+      if (photos && photos.length > 0) {
+        const photoUrl = photos[0].getUrl({
+          maxWidth: 200,
+          maxHeight: 200,
+        });
+        const thumbnailImage = $("<img>").attr("src", photoUrl).css({
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+        });
+        item.append(thumbnailImage);
+      }
+
+      // Display place name, address, and rating
+      const details = $("<div class='restaurant-details'></div>");
+      details.append(`<strong>${name}</strong><br>`);
+      details.append(`<span>${address}</span><br>`);
+      details.append(`Rating: ${rating}`);
+
+      item.append(details); // Append to item
+      col.append(item); // Append to column
+      row.append(col); // Append to row
     });
 
-    $(document).on("mouseenter", ".restaurant-item, .featured-item", function () {
-      const placeInfo = getPlaceInfo($(this).data("place-id")); // Fetch additional information for the place (e.g., contact details)
-      showInfoBox($(this), placeInfo);
-    });
+    container.append(row); // Append the row to the container
 
-    $(document).on("mouseleave", ".restaurant-item, .featured-item", function () {
-      hideInfoBox();
-    });
-
-    const placesService = new google.maps.places.PlacesService(map);
-
-    // Handle the "Search" button click event
-    $("#searchButton").click(function () {
-      const locationInput = $("#searchInput").val();
-
-      // Perform a Geocoding request to convert the location input to coordinates
-      const geocoder = new google.maps.Geocoder();
-      geocoder.geocode({ address: locationInput }, function (results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-          const location = results[0].geometry.location;
-
-          // Define the search request for restaurants
-          const request = {
-            location: location,
-            radius: 5000,
-            types: ["restaurant"],
-          };
-
-          // Create a Bootstrap row for the grid
-          const row = $("<div class='row'></div>");
-          const featuredPlacesSection = $("<div class='col-md-12 mt-4'></div>");
-          featuredPlacesSection.html("<h2>- Featured Places -</h2>");
-
-          const featuredRequest = {
-            location: location,
-            radius: 5000, // Adjust the radius as needed
-            types: ["restaurant"], // Adjust the types as needed
-            keyword: "point_of_interest", // Add a keyword for featured places
-          };
-
-          placesService.nearbySearch(
-            featuredRequest,
-            function (results, status) {
-              if (status === google.maps.places.PlacesServiceStatus.OK) {
-                // Randomize featured places
-                const shuffleFeaturedResults = shuffleArray(results);
-                // Create a Bootstrap row for featured places
-                const featuredRow = $("<div class='row'></div>");
-                // Limit the number of featured places to 3
-                const featuredResults = results.slice(0, 3);
-
-                $.each(featuredResults, function (index, place) {
-                  const col = $("<div class='col-md-4'></div>");
-                  const featuredItem = $("<div class='featured-item'></div>");
-                  featuredItem.css("height", "200px");
-                  const featuredName = place.name;
-                  const featuredAddress = place.vicinity;
-                  const featuredRating = place.rating;
-                  const featuredPhotos = place.photos;
-
-                  // Create and append featured place list items
-                  const listItem = $("<div class='featured-item'></div>");
-
-                  if (featuredPhotos && featuredPhotos.length > 0) {
-                    const photoUrl = featuredPhotos[0].getUrl({
-                      maxWidth: 200,
-                      maxHeight: 200,
-                    });
-                    const thumbnailImage = $("<img>")
-                      .attr("src", photoUrl)
-                      .css({
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover", // Maintain aspect ratio and cover the container
-                      });
-                    featuredItem.append(thumbnailImage);
-                  }
-                  // Display featured place name, address, and rating
-                  const details = $("<div class='restaurant-details'></div>");
-                  details.append(`<strong>${featuredName}</strong><br>`);
-                  details.append(`<span>${featuredAddress}</span><br>`);
-                  details.append(`Rating: ${featuredRating}`);
-
-                  featuredItem.append(details); // Append to featuredItem
-
-                  col.append(featuredItem); // Append to column
-                  featuredRow.append(col); // Append to featuredRow
-                });
-                // Append the featured places row to the featuredPlacesSection
-                featuredPlacesSection.append(featuredRow);
-                featuredPlacesSection.append("<hr>");
-              } else {
-                console.error(
-                  "Places API request for featured places failed:",
-                  status
-                );
-              }
-            }
-          );
-
-          row.append(featuredPlacesSection);
-
-          const searchTitle = $("<h2></h2>");
-          searchTitle.text(`- Places near ${locationInput} -`);
-          row.append(searchTitle); // Append the title
-
-          // Perform the Places API search
-          placesService.nearbySearch(request, function (results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-              // Render the list of restaurants
-              const restaurantList = $("#restaurantList");
-              restaurantList.empty();
-
-              $.each(results, function (index, place) {
-                const col = $("<div class='col-md-4'></div>");
-                const restaurantItem = $("<div class='restaurant-item'></div>");
-                restaurantItem.css("height", "150px");
-                const restaurantName = place.name;
-                const restaurantAddress = place.vicinity;
-                const restaurantRating = place.rating;
-                const photos = place.photos;
-
-                // Create and append restaurant list items
-                const listItem = $("<div class='restaurant-item'></div>");
-
-                if (photos && photos.length > 0) {
-                  const photoUrl = photos[0].getUrl({
-                    maxWidth: 200,
-                    maxHeight: 200,
-                  });
-                  const thumbnailImage = $("<img>").attr("src", photoUrl).css({
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover", // Maintain aspect ratio and cover the container
-                  });
-                  restaurantItem.append(thumbnailImage);
-                }
-
-                // Display restaurant name, address, and rating
-                const details = $("<div class='restaurant-details'></div>");
-                details.append(`<strong>${restaurantName}</strong><br>`);
-                details.append(`<span>${restaurantAddress}</span><br>`);
-                details.append(`Rating: ${restaurantRating}`);
-
-                restaurantItem.append(details); // Append to restaurantItem
-
-                col.append(restaurantItem); // Append to column
-                row.append(col); // Append to row
-              });
-
-              restaurantList.append(row); // Append the row to the restaurantList container
-            } else {
-              console.error("Places API request failed:", status);
-            }
-          });
-        } else {
-          console.error("Geocoding request failed:", status);
-        }
-      });
-    });
-  });
-}
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    // Add a title for the section
+    if (title) {
+      const sectionTitle = $("<h2></h2>");
+      sectionTitle.text(title);
+      container.prepend(sectionTitle); // Prepend the title
+    }
   }
-  return array;
-}
 
-function getPlaceInfo(placeId) {
-  // You can make an API request here to fetch additional information
-  // For demonstration purposes, let's assume it's a contact phone number
-  // Replace this with your actual API call
-  return "Contact: 123-456-7890";
-}
+  // Function to fetch and display places based on a search request
+  function fetchAndDisplayPlaces(request, container, itemClass, itemHeight, title) {
+    placesService.nearbySearch(request, function (results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        createPlaceList(container, results, itemClass, itemHeight, title);
+      } else {
+        console.error("Places API request failed:", status);
+      }
+    });
+  }
 
-// Function to show the floating info box
-function showInfoBox(item, info) {
-  const infoBox = $("#infoBox");
-  const offset = item.offset();
-  const height = item.height();
+  // Handle the "Search" button click event
+  $("#searchButton").click(function () {
+    const locationInput = $("#searchInput").val();
 
-  infoBox.html(info);
+    // Perform a Geocoding request to convert the location input to coordinates
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: locationInput }, function (results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        const location = results[0].geometry.location;
 
-  infoBox.css({
-    top: offset.top + height + 10, // Adjust the position as needed
-    left: offset.left,
+        // Define the search request for restaurants
+        const restaurantRequest = {
+          location: location,
+          radius: 5000,
+          types: ["restaurant"],
+        };
+
+        // Define the search request for featured places
+        const featuredRequest = {
+          location: location,
+          radius: 5000,
+          types: ["restaurant"],
+          keyword: "point_of_interest",
+        };
+
+        const restaurantList = $("#restaurantList");
+        const featuredPlacesList = $("#featuredPlacesList");
+
+        // Fetch and display restaurant places
+        fetchAndDisplayPlaces(restaurantRequest, restaurantList, "restaurant-item", "150px", `- Places near ${locationInput} -`);
+
+        // Fetch and display featured places
+        fetchAndDisplayPlaces(featuredRequest, featuredPlacesList, "featured-item", "200px", "- Featured Places -");
+      } else {
+        console.error("Geocoding request failed:", status);
+      }
+    });
   });
 
-  infoBox.show();
-}
+  // Handle mouse enter and leave events for .restaurant-item and .featured-item
+  $(document).on("mouseenter mouseleave", ".restaurant-item, .featured-item", function (event) {
+    const placeInfo = getPlaceInfo($(this).data("place-id"));
+    showInfoBox($(this), placeInfo, event.type === "mouseenter");
+  });
 
-// Function to hide the floating info box
-function hideInfoBox() {
-  $("#infoBox").hide();
+  function showInfoBox(item, info, show) {
+    const infoBox = $("#infoBox");
+    const offset = item.offset();
+    const height = item.height();
+
+    infoBox.html(info);
+
+    if (show) {
+      infoBox.css({
+        top: offset.top + height + 10,
+        left: offset.left,
+      });
+      infoBox.show();
+    } else {
+      infoBox.hide();
+    }
+  }
+
+  function getPlaceInfo(placeId) {
+    // You can make an API request here to fetch additional information
+    // For demonstration purposes, let's assume it's a contact phone number
+    // Replace this with your actual API call
+    return "Contact: 123-456-7890";
+  }
+});
 }
